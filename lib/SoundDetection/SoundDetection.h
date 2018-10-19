7428521fa115bdabc4ef76e26ce7e59ee3bd5952
Alexei Sersun <alex.sersun@yahoo.com>
2018-10-19 11:32:32 +0300

    Extract sound detection sensor.

---
+++ lib/SoundDetection/SoundDetection.h
@@ -0,0 +1,10 @@
+#ifndef SOUND_DETECTION_H_
+#define SOUND_DETECTION_H_
+
+#include <Arduino.h>
+#include <RobotDefinitions.h>
+#include <Sensor.h>
+
+extern struct binary_sensor_str sensorSoundDet;
+
+#endif
